9b0e6e8f7be6662cb5fb5191c4208dd6c322fe8b
AzadMohammadi-z <azadmohammadi74@gmail.com>
2018-12-13 22:37:45 +0330

    Updates

---
+++ precache-manifest.c2553c7277dac2bed96d8f36003983fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "d98b9a4d5de93ec5e8c2",
+    "url": "/resume/static/js/main.d98b9a4d.chunk.js"
+  },
+  {
+    "revision": "b2a7456e2142ccb1f19d",
+    "url": "/resume/static/js/1.b2a7456e.chunk.js"
+  },
+  {
+    "revision": "d98b9a4d5de93ec5e8c2",
+    "url": "/resume/static/css/main.6d117eb4.chunk.css"
+  },
+  {
+    "revision": "b2a7456e2142ccb1f19d",
+    "url": "/resume/static/css/1.96f72343.chunk.css"
+  },
+  {
+    "revision": "2795213aa63bc851d2949c573ab012b3",
+    "url": "/resume/index.html"
+  }
+];
